6e00947f0a6f667a31b7739857f680b73d616b12
Kevin Yu <kevinyu05062006@gmail.com>
2020-05-14 18:26:14 +0800

    Introduce open-graph-scraper

--- src/lib/unfurl.js
+++ src/lib/unfurl.js
@@ -1,1 +1,9 @@
-const test = "https://www.ettoday.net/dalemon/post/40959";
+const ogs = require('open-graph-scraper');
+const options = {'url': 'https://www.ettoday.net/dalemon/post/40959'};
+
+ogs(options, (error, results) => {
+    // This is returns true or false. True if there was a error. The error it self is inside the results object.
+    console.log('error:', error);
+    console.log('results:', results);
+});
+   
